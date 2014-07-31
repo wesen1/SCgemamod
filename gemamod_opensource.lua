22fa9f9ecae39470a6e9a02341e1d8ab6c7efb3c
Jg99 <jamesgryffindor99@gmail.com>
2014-07-31 15:58:50 -0600

    Update gemamod_opensource.lua
    Fixed !addtime

--- gemamod_opensource.lua
+++ gemamod_opensource.lua
@@ -303,14 +303,14 @@ say("\fR Lua AC Executables made by Sveark. \f9Our website is \f4www.sctechusa.c
     { false, false, false };
     function (cn, args)
       if tonumber(args[1]) < 16 and tonumber(args[1]) > 2 then
-      
       settimeleft(tonumber(args[1]))
-       -- say(string.format("\fR [SERVER INFO] \f4 Time remaining changed to %d", args[1])) -- commented out due to potential spam
+       say(string.format("\fR [SERVER INFO] \f4 Time remaining changed to %d", args[1]), cn)
         else
-     --   say(string.format("\f3 ERROR: Time has to be between 3 and 15 minutes, %s!", getname(cn)))$
+        say(string.format("\f3 ERROR: Time has to be between 3 and 15 minutes, %s!", getname(cn)), cn)
 end
     end
   };
+
 
 --]]
   ["!ext"] = 
